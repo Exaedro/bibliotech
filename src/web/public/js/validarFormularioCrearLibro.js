8033fa78e06e42a13d25c1367417ba00a882f7d0
Exaedro <owomolo123@gmail.com>
2024-09-04 21:02:04 -0300

    validar formulario de crear libro creado

--- src/web/public/js/validarFormularioCrearLibro.js
+++
@@ -1,6 +0,0 @@
-const botonCrearLibro = document.getElementById('botonCrearLibro')
-const crearLibroForm = document.getElementById('crearLibroForm')
-
-botonCrearLibro.addEventListener('click', (elem) => {
-    
-})
