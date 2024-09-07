89556e7d3b0a781d4d3863f86368cb7534c0fa3f
Exaedro <owomolo123@gmail.com>
2024-09-07 14:37:50 -0300

    verificador para saber si la base de datos esta encendida añadido

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,10 @@ import cors from 'cors'
 
 export const appApi = express()
 
+// Base de datos
+import db from './database.js'
+db.authenticate()
+
 // Configuracion
 appApi.set('PORT', 3000 || process.env.PORT)
 appApi.set('json spaces', 2)
