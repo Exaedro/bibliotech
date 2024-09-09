ec370a49b71275f9fee88aed7e71a4365ffef6ba
Exaedro <owomolo123@gmail.com>
2024-09-09 16:20:52 -0300

    error al eliminar un usuario desde el panel arreglado

--- src/web/utils/validators.js
+++ src/web/utils/validators.js
@@ -4,7 +4,7 @@ export const createBookValidator = [
     body('title').isLength({ min: 3 }).withMessage('Escribe mas de tres caracteres como mínimo'),
     body('author').isLength({ min: 3 }).withMessage('Escribe mas de tres caracteres como mínimo'),
     body('isbn').isISBN().withMessage('El ISBN no es válido'),
-    body('date').isISO8601().withMessage('La fecha no es válida'),
+    body('date').isLength({ min: 3 }).withMessage('La fecha no es válida'),
     body('pages').isInt({ min: 1 }).withMessage('La cantidad de páginas debe ser un número entero mayor que 0'),
     body('language').isLength({ min: 4 }).withMessage('El idioma debe tener al menos 4 caracteres'),
     body('publisher').isLength({ min: 4 }).withMessage('La editorial debe tener al menos 4 caracteres'),
